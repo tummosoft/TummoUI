07e3506429ae828d93509f65085ab0c6493fd31d
Nguyen Van Hien <hien.ho.phong@gmail.com>
2023-02-01 16:03:25 +0700

    V11

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 function randomID() {
     var str = 'qwertyuiopasdfghjklzxcvbnm1234567890';
     var vID = '';
+    
     for (var i=0; i < 5; i++) {
         var rIndex =Math.floor(Math.random() * str.length);
         vID = vID + str.substring(rIndex, rIndex + 1);
     }
     return vID;
+}
+
+function checkid(e) {
+    var cid = $(e).parent().attr('id');
+    if (cid == undefined) {
+            var newid = randomID();
+            $(e).parent().attr("id",newid);            
+    }
 }
